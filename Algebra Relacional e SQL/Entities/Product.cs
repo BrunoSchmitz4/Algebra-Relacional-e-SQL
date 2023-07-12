6c7cc9b8c475b03bda05edd7103ca7045caadabf
Bruno Silva <brunosch250@gmail.com>
2023-07-12 11:55:45 -0300

    Initial commit

---
+++ Algebra Relacional e SQL/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Algebra_Relacional_e_SQL.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"|Nome: {Name} | Preço: R$ {Price}|";
+        }
+    }
+}
