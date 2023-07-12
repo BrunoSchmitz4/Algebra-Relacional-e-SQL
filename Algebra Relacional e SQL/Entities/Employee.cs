b4725eef4d3f10d0cd88f3c468cc6a300ae55ef8
Bruno Silva <brunosch250@gmail.com>
2023-07-12 13:33:44 -0300

    Update

---
+++ Algebra Relacional e SQL/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Algebra_Relacional_e_SQL.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"| Nome: {Name} | Email: {Email} | Salário: R$ {Salary}";
+        }
+    }
+}
--- Algebra Relacional e SQL/Program.cs
+++ Algebra Relacional e SQL/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 
 namespace Algebra_Relacional_e_SQL
 {
@@ -11,9 +12,42 @@ namespace Algebra_Relacional_e_SQL
     {
         static void Main(string[] args)
         {
-            AlgebraAndSQL();
+            Exercise();
         }
 
+        static void Exercise()
+        {
+            Console.Write("Caminho do arquivo: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees_list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                    employees_list.Add(new Employee(name, email, salary));
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Informações solicitadas: ");
+            Console.WriteLine("Valor de salário mínimo p/ busca de email: ");
+            double minSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            var emails = employees_list.Where(e => e.Salary > minSalary).OrderBy(e => e.Email).Select(e => e.Email);
+            Console.WriteLine("================================");
+            foreach (string email in emails)
+            {
+                Console.Write($"| {email}\n");
+            }
+            Console.WriteLine("================================");
+            var names = employees_list.Where(e => e.Name.StartsWith('M')).Select(e => e.Salary).Sum();
+            Console.WriteLine("| Sum of salary whose name starts with M: R$ " + names.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("================================");
+        }
         static void AlgebraAndSQL()
         {
             Console.Write("Enter full file path: ");
